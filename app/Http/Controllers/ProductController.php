67127ec8c639f161800c2a66a72e1faa2710bc42
Rubs <hyperfury409@gmail.com>
2024-02-21 09:56:42 +0800

    Final Output

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,18 +11,6 @@ class ProductController extends Controller
         $themes = [
             'Gadgets' => [ 
                 'iPhone 15', 'Samsung Galaxy S24', 'Google Pixel 8'
-            ],
-            'Books' => [
-                'Harry Potter', 'The Subtle Art of Not Giving a F*ck', 'The 4-Hour Workweek'
-            ],
-            'Movies' => [
-                'The Godfather', 'Pulp Fiction', 'The Shawshank Redemption'
-            ],
-            'Anime'   => [
-                'Attack on Titan', 'Fullmetal Alchemist: Brotherhood', 'Death Note'
-            ],
-            'Restaurants' => [
-                'Jollibee', "McDonald's", 'KFC'
             ]
         ];
 
