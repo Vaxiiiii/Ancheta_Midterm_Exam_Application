6fc2b58aa0a847948351be7fda49e638e78c06db
Rubs <hyperfury409@gmail.com>
2024-02-21 09:37:37 +0800

    adding Controller and defining Products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,31 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function displayProducts(Request $request, $theme)
+    {
+        $themes = [
+            'Gadgets' => [ 
+                'iPhone 15', 'Samsung Galaxy S24', 'Google Pixel 8'
+            ],
+            'Books' => [
+                'Harry Potter', 'The Subtle Art of Not Giving a F*ck', 'The 4-Hour Workweek'
+            ],
+            'Movies' => [
+                'The Godfather', 'Pulp Fiction', 'The Shawshank Redemption'
+            ],
+            'Anime'   => [
+                'Attack on Titan', 'Fullmetal Alchemist: Brotherhood', 'Death Note'
+            ],
+            'Restaurants' => [
+                'Jollibee', "McDonald's", 'KFC'
+            ]
+        ];
+
+        if (!array_key_exists($theme, $themes)) {
+            abort(404); // Not found
+        }
+
+        $products = $themes[$theme];
+        return view('products.index', compact('products', 'theme'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/products/{theme}', [ProductController::class, 'displayProducts']);
 
